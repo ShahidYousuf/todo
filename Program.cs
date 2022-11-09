4bb3db584a6d113cefcc5fbf65f872a9c1ad171f
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-11-09 22:08:55 +0530

    sample command line arguments

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ class Application
     static void Main()
     {
         Console.WriteLine("Welcome to todos, the current time is " + DateTime.Now);
+        string[] arguments = Environment.GetCommandLineArgs();
+        Console.WriteLine("Command Line arguments: {0}", string.Join(", ", arguments));
     }
 
 }
