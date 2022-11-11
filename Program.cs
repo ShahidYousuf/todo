97f5d07818d8cdcc63c63464e4919869fc47b0e1
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-11-11 18:09:17 +0530

    add basic Todo class

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using todo;
 class Application 
 {
     static void Main()
     {
         Console.WriteLine("Welcome to todos, the current time is " + DateTime.Now);
         string[] arguments = Environment.GetCommandLineArgs();
+        Persistence persistence = Persistence.FILE;
+        Console.WriteLine("The persistence is of type {0}", persistence);
         Console.WriteLine("Command Line arguments: {0}", string.Join(", ", arguments));
+
+        Todo todo = new Todo("First todo created");
+        todo.Id = 10;
+        Console.WriteLine("{0}", todo);
+    }
+
+    enum Persistence
+    {
+        FILE,
+        DB,
     }
 
 }
---
+++ Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace todo
+{
+    public class Todo
+    {
+        public Todo(string title)
+        {
+            Title = title;
+            Completed = false;
+        }
+
+        public string Title { get; set; }
+
+        public int Id { get; set; }
+
+        public bool Completed { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", Id, Title, Completed);
+        }
+    }
+}
+
