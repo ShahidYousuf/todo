8f2c2628318c07f1c68b8190b9788b9180b7c78a
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-11-12 12:32:07 +0530

    add command formatting for get, create and list

--- Program.cs
+++ Program.cs
@@ -53,6 +53,8 @@ class Application
         OUTPUT,
         INDEX,
         TITLE,
+        COMMAND,
+        NONE,
     }
 
     class CommandParser
@@ -67,7 +69,7 @@ class Application
         {
            
             public Command Command { get; set; }
-            public Dictionary<string, string> OptionValues { get; set; }
+            public Dictionary<CommandOption, string> OptionValues { get; set; }
 
         }
         public BaseCommandWithOptions CommandWithOptions()
@@ -76,8 +78,8 @@ class Application
             // todo edit -i 5 -t "hello world"
 
             BaseCommandWithOptions commandWithOptions = new BaseCommandWithOptions();
-            Dictionary<string, string> map = new Dictionary<string, string>();
-            map.Add("", "");
+            Dictionary<CommandOption, string> map = new Dictionary<CommandOption, string>();
+            //map.Add(CommandOption.NONE, "");
             commandWithOptions.OptionValues = map;
             switch (Arguments.Length)
             {
@@ -89,30 +91,30 @@ class Application
                     break;
                 case 3:
                     commandWithOptions.Command = Command.HELP;
-                    map.Add("-c", Arguments[1]);
+                    map.Add(CommandOption.COMMAND, Arguments[1]);
                     commandWithOptions.OptionValues = map;
                     break;
                 case 4:
                     commandWithOptions.Command = GetCommand();
-                    map.Add(Arguments[2], Arguments[3]);
+                    map.Add(GetCommandOption(Arguments[2]), Arguments[3]);
                     commandWithOptions.OptionValues = map;
                     break;
                 case 5:
                     commandWithOptions.Command = Command.HELP;
-                    map.Add("-c", Arguments[1]);
+                    map.Add(CommandOption.COMMAND, Arguments[1]);
                     commandWithOptions.OptionValues = map;
                     break;
                 case 6:
                     commandWithOptions.Command = GetCommand();
-                    map.Add(Arguments[2], Arguments[3]);
-                    map.Add(Arguments[4], Arguments[5]);
+                    map.Add(GetCommandOption(Arguments[2]), Arguments[3]);
+                    map.Add(GetCommandOption(Arguments[4]), Arguments[5]);
                     commandWithOptions.OptionValues = map;
                     break;
                 default:
                     commandWithOptions.Command = Command.HELP;
                     break;
             }
-            return commandWithOptions;
+            return FormatBaseCommandWithOptions(commandWithOptions);
                 
         }
 
@@ -139,6 +141,114 @@ class Application
                     return Command.HELP;
                 default:
                     return Command.HELP;
+            }
+        }
+
+        private CommandOption GetCommandOption(string option)
+        {
+            switch (option)
+            {
+                case "-o":
+                case "--output":
+                    return CommandOption.OUTPUT;
+                case "-i":
+                case "--index":
+                    return CommandOption.INDEX;
+                case "-t":
+                case "--title":
+                    return CommandOption.TITLE;
+                case "-c":
+                case "--command":
+                    return CommandOption.COMMAND;
+                default:
+                    return CommandOption.NONE;
+            }
+        }
+
+        private BaseCommandWithOptions FormatBaseCommandWithOptions(BaseCommandWithOptions commandWithOptions)
+        {
+            Command command = commandWithOptions.Command;
+            var optionValues = commandWithOptions.OptionValues;
+            switch (command)
+            {
+                case Command.LIST:
+                    string value = "";
+                    if (optionValues.Count > 1)
+                    {
+                        optionValues.Clear();
+                        command = Command.HELP;
+                        optionValues.Add(CommandOption.COMMAND, "list");
+                    }
+                    if (optionValues.TryGetValue(CommandOption.OUTPUT, out value)) {
+                        if (value == "completed" || value == "pending") {
+                            optionValues.Clear();
+                            optionValues.Add(CommandOption.OUTPUT, value);
+                        }else
+                        {
+                            command = Command.HELP;
+                            optionValues.Clear();
+                            optionValues.Add(CommandOption.COMMAND, "list");
+                        }
+
+                    }
+                    commandWithOptions.Command = command;
+                    commandWithOptions.OptionValues = optionValues;
+                    return commandWithOptions;
+                case Command.GET:
+                    string gvalue = "";
+                    if (optionValues.Count > 1)
+                    {
+                        optionValues.Clear();
+                        command = Command.HELP;
+                        optionValues.Add(CommandOption.COMMAND, "get");
+                    }
+                    if (optionValues.TryGetValue(CommandOption.INDEX, out gvalue))
+                    {
+                        var isNumeric = int.TryParse(gvalue, out _);
+                        if (isNumeric)
+                        {
+                            optionValues.Clear();
+                            optionValues.Add(CommandOption.INDEX, gvalue);
+                        }else
+                        {
+                            optionValues.Clear();
+                            command = Command.HELP;
+                            optionValues.Add(CommandOption.COMMAND, "get");
+                        }
+                    } else
+                    {
+                        optionValues.Clear();
+                        command = Command.HELP;
+                        optionValues.Add(CommandOption.COMMAND, "get");
+                    }
+                    commandWithOptions.Command = command;
+                    commandWithOptions.OptionValues = optionValues;
+                    return commandWithOptions;
+                case Command.CREATE:
+                    string cvalue = "";
+                    if (optionValues.Count > 1)
+                    {
+                        optionValues.Clear();
+                        command = Command.HELP;
+                        optionValues.Add(CommandOption.COMMAND, "create");
+                    }
+                    if (optionValues.TryGetValue(CommandOption.TITLE, out cvalue))
+                    {
+                        optionValues.Clear();
+                        optionValues.Add(CommandOption.TITLE, cvalue);              
+                    }
+                    else
+                    {
+                        optionValues.Clear();
+                        command = Command.HELP;
+                        optionValues.Add(CommandOption.COMMAND, "create");
+                    }
+                    commandWithOptions.Command = command;
+                    commandWithOptions.OptionValues = optionValues;
+                    return commandWithOptions;
+
+                default:
+                    return commandWithOptions;
             }
         }
 
