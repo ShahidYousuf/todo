0607c0300e2cb035c8e785a62d9ebedc7de4b387
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-11-14 22:21:23 +0530

    add refresh db function

---
+++ Store.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+namespace todo
+{
+    public class Store
+    {
+        string? HomeDirPath = "";
+        string TODO_DB = Path.DirectorySeparatorChar + ".todo.db";
+        string TODO_INDEX = Path.DirectorySeparatorChar + ".todo_index.db";
+
+        string DBPath;
+        string DBIndexPath;
+        int Index { get; set; }
+        
+        public Store()
+        {
+            HomeDirPath = Environment.GetEnvironmentVariable("HOME");
+            if (HomeDirPath == null)
+            {
+                throw new Exception("Missing Environment variable `HOME`, have you set it?");
+            }
+            DBPath = HomeDirPath + TODO_DB;
+            DBIndexPath = HomeDirPath + TODO_INDEX;
+            InitFileDB();
+        }
+
+        private void InitFileDB()
+        {
+            if (HomeDirPath is not null)
+            {
+
+                FileStream db_file = File.Open(DBPath, FileMode.OpenOrCreate);
+                FileStream index_file = File.Open(DBIndexPath, FileMode.OpenOrCreate, FileAccess.Read);
+                StreamReader reader = new StreamReader(index_file);
+                string? readIndex = reader.ReadLine();
+              
+                if (readIndex is not null)
+                {
+                    int.TryParse(readIndex, out int index);
+                    Index = index;
+                 
+                } else
+                {
+                    Index = 0;
+                }
+                reader.Close();
+                db_file.Close();
+               
+            }
+        }
+
+
+        public void WriteTodo(Todo todo)
+        {
+            FileStream db_file_stream = File.Open(DBPath, FileMode.Append, FileAccess.Write);
+            todo.Id = Index + 1;
+            StreamWriter writer = new StreamWriter(db_file_stream);
+            writer.WriteLine("{0}", todo);
+            writer.Close();
+            db_file_stream.Close();
+            SaveIndex();
+        }
+
+        public void SaveIndex()
+        {
+            FileStream db_index_stream = File.Open(DBIndexPath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(db_index_stream);
+            writer.WriteLine(Index + 1);
+            writer.Close();
+            db_index_stream.Close();
+        }
+
+        public List<Todo> GetTodos()
+        {
+            FileStream db_file_stream = File.Open(DBPath, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(db_file_stream);
+            List<Todo> todos = new List<Todo>();
+            while (!reader.EndOfStream)
+            {
+                string? data = reader.ReadLine();
+                if (data is null)
+                {
+                    break;
+                }
+                string[] parts = data.Split(" ");
+                int.TryParse(parts[0], out int index);
+                string title = parts[1];
+                bool.TryParse(parts[2], out bool completed);
+                Todo todo = new Todo(title);
+                todo.Id = index;
+                todo.Completed = completed;
+                todos.Add(todo);
+            }
+            reader.Close();
+            db_file_stream.Close();
+            return todos;
+
+        }
+
+        public Todo? GetTodo(int id)
+        {
+            string index = id.ToString();
+            FileStream db_file_stream = File.Open(DBPath, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(db_file_stream);
+            while (!reader.EndOfStream)
+            {
+                string? data = reader.ReadLine();
+                if (data is null)
+                {
+                    break;
+                }
+                string[] parts = data.Split(" ");
+                if (index == parts[0])
+                {
+                    Todo todo = new Todo(parts[1]);
+                    todo.Id = id;
+                    bool.TryParse(parts[2], out bool completed);
+                    todo.Completed = completed;
+                    return todo;
+                }
+            }
+            reader.Close();
+            db_file_stream.Close();
+            return null;
+        }
+
+        public void Refresh_DB(List<Todo> todos)
+        {
+            todos = todos.OrderBy(t => t.Id).ToList();
+            FileStream db_file_stream = File.Open(DBPath, FileMode.Open, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(db_file_stream);
+            foreach (var todo in todos)
+            {
+                writer.WriteLine("{0}", todo);
+            }
+            writer.Close();
+            db_file_stream.Close();
+        }
+
+
+
+
+
+    }
+}
+
